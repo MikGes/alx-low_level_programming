3d015754db468c29ed6bd30f72e384eb86d57622
MikGes <mikiyasgetnet6@gmail.com>
2022-09-08 08:41:38 -0700

    d

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
-
+#include <studio.h>
 /**
- * main - Entry point
- *  
- * Return:0 after printing the function 
- */
+*main-print the string
+*
+*Description:using the main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return 0;
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
 }
