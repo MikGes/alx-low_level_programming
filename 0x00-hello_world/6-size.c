a38a8d02a94bc597616d06ab200764c69d4d3224
MikGes <mikiyasgetnet6@gmail.com>
2022-09-08 07:46:54 -0700

    c programming

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	printf("Size of a char: %lu byte(s)",sizeof(a));
+	printf("Size of an int: %lu byte(s)",sizeof(b));
+	printf("Size of a long int: %lu byte(s)",sizeof(c));
+	printf("Size of a long long: %lu byte(s)",sizeof(d));
+	printf("Size of a float: %lu byte(s)\n",sizeof(e));
+	    return (0);
+}
