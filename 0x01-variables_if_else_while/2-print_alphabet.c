0560ece342737c3929e0ed89495a66a11338eeb2
MikGes <mikiyasgetnet6@gmail.com>
2022-09-09 08:45:59 -0700

    algo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - print alphabet
+*Return: 0(success)
+*/
+int main(void)
+{
+	char a;
+
+for (a = 'a' ; a <= 'z' ; a++)
+	putchar(a);
+	putchar("\n");
+	return (0);
+}
