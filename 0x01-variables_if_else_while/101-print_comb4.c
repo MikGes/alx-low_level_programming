188f01daf54dabe587625a73920d3134d4a0ec61
MikGes <mikiyasgetnet6@gmail.com>
2022-09-09 14:11:41 -0700

    algo commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+*main - print alphabet
+*Return: 0(success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+for (i = 0 ; i <= 7 ; i++)
+{
+	for (j = 1 ; j <= 8 ; j++)
+	{
+		for (k = 2 ; k <= 9 ; k++)
+		{
+			if (k > j && j > i)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i + j + k != 24)
+					putchar(',');
+					putchar(' ');
+			}
+		}
+	}
+}
+putchar('\n');
+return (0);
+}
