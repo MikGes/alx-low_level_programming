82de131fc8ee00f7a79d82cc7c3cbdfe0da9aedb
MikGes <mikiyasgetnet6@gmail.com>
2022-09-09 13:07:22 -0700

    algo commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - print alphabet
+*Return: 0(success)
+*/
+int main(void)
+{
+	int i;
+for (i = 0 ; i <= 9 ; i++)
+{
+	putchar(i + '0');
+	if (i != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+}
+	return (0);
+}
