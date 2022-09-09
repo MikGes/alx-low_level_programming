29c7f26433bcb41c8eeff8657aa6c26495354f87
MikGes <mikiyasgetnet6@gmail.com>
2022-09-09 09:43:27 -0700

    algo commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - print alphabet
+*Return: 0(success)
+*/
+int main(void)
+{
+	auto i;
+
+for (i = '0' ; i < '10' ; i++)
+	putchar(i);
+putchar('\n');
+return (0);
+}
