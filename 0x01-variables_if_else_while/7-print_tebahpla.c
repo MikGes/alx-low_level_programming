f89e861b7ab7b6a0c686df8cf595913a8bf02d38
MikGes <mikiyasgetnet6@gmail.com>
2022-09-09 12:33:45 -0700

    algo commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - print alphabet
+*Return: 0(success)
+*/
+int main(void)
+{
+	char i;
+
+for (i = 'z' ; i >= 'a' ; i--)
+	putchar(a);
+putchar('\n');
+return (0);
+}
