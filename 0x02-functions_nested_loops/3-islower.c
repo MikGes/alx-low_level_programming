ea6e64f612ee3d9cc033e371ad599a7a465ea06e
MikGes <mikiyasgetnet6@gmail.com>
2022-09-13 10:00:41 -0700

    This is a commit on 0x02 fuction loop and nest project

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+*_islower - check if it is lower or not
+*@c: character to check
+*Return: 0 if c is not lowecase and 1 otherwise
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
