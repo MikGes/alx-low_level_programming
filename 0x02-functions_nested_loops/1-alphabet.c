5afa506fc69bf9c01f19f8651bbc29fa3cfb917f
MikGes <mikiyasgetnet6@gmail.com>
2022-09-13 09:42:03 -0700

    This is a commit on 0x02 fuction loop and nest project

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet - print alphabet
+*Return: 0(success)
+*/
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
