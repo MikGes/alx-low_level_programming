94dc37ce47d449d9abd8a5e818750bf2be2f492e
MikGes <mikiyasgetnet6@gmail.com>
2022-09-13 09:33:32 -0700

    This is a commit on 0x02 fuction loop and nest project

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,18 +2,18 @@
 
 /**
 *main - print alphabet
-*Return: 0(success)
+*Return: Always 0(success)
 */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
