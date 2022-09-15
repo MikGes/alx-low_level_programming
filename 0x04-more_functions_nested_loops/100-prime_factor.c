36a3b86d1a904bb9e48b04d4ecafb81b6e00cd13
MikGes <mikiyasgetnet6@gmail.com>
2022-09-15 13:02:00 -0700

    This is a commit on 0x04 more nested loops tasks

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
+#include<math.h>
 /**
  * main - prints the largest prime number of 612852475143
  * Return: 0(success)
  */
 int main(void)
 {
-	long int i;
+	long int n;
 	long int max;
+	long int i;
 
 	n = 612852475143;
 	max = -1;
