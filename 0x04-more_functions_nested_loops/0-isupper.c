58723e01f216b81128bd75e556743504d0d38f11
MikGes <mikiyasgetnet6@gmail.com>
2022-09-15 08:41:07 -0700

    This is a commit on 0x04 more nested loops tasks

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,14 +2,11 @@
 #include <stdio.h>
 
 /**
- * int _isupper - check the code.
+ * _isupper - check the code.
  * @c: the xharacter to be checked
  * Return: 0 if the c is lowercase and returns 1 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-		return (1);
-	else if (c >= 97 && c <= 122)
-		return (0);
+	return (c >= 'A' && c <= 'Z');
 }
