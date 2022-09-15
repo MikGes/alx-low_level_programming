c97160c0f991cad9e65b24c8fa9cb077d8184f71
MikGes <mikiyasgetnet6@gmail.com>
2022-09-15 09:15:17 -0700

    This is a commit on 0x04 more nested loops tasks

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main"
+/**
+ * print_most_numbers - prints numebrs from 0 to 9 except 2 and 4
+ * Return: print nums
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+	}
+	_putchar'\n');
+}
