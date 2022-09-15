fdaeb41cc4169c4119afe8eab87a633c3671f4ae
MikGes <mikiyasgetnet6@gmail.com>
2022-09-15 11:03:54 -0700

    This is a commit on 0x04 more nested loops tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include<stdio.h>
 
 /**
  * print_diagonal - prints diagonal for n times
@@ -28,7 +27,7 @@ else
 			for (j = 1; j <= i; j++)
 			{
 				_putchar(' ');
-				_ptchar('\\');
+				_putchar('\\');
 			}
 			_putchar('\n');
 		}
