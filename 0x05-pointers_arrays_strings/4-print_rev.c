1c7f30acf606786ce0719a2b94ac89d7b997dcfc
MikGes <mikiyasgetnet6@gmail.com>
2022-09-19 07:36:55 -0700

    This is a commit on pointers and arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,7 +5,7 @@
  */
 void print_rev(char *s)
 {
-	int i, j;
+	int i, j, len;
 
 	i = 0;
 	while (s[i] != '\0')
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,7 @@ void puts2(char *str)
 	int i, len;
 
 	len = 0;
-	while (str[len] != '\n')
+	while (str[len] != '\0')
 		len++;
 	for ( i = 0; i < len; i = i + 2)
 	{
