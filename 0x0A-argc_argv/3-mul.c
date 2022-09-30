27f3049b43a350e38b38c4bdc8f6fff44e6287e5
MikGes <mikiyasgetnet6@gmail.com>
2022-09-30 08:00:09 -0700

    commit on argv projects

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int result;
+	int result, num1, num2;
 
 
 	if (argc < 3 || argc > 3)
@@ -18,11 +18,11 @@ int main(int argc, char *argv[])
 	}
 	else
 	{
-		int num1 = atol(argv[1]);
-		int num2 = atol(argv[2]);
+		num1 = atol(argv[1]);
+		num2 = atol(argv[2]);
 
-
-		printf("%d\n", num1 * num2);
+		result = num1 * num2;
+		printf("%d\n", result);
 	}
 	return (0);
 }
